5fe54f9b01e575ee3b5b3ce75e60c59418a4557b
MauricioMartinez <maoworshipper@gmail.com>
2023-04-29 17:26:16 -0500

    Add aditional configurations to organization chart view

---
+++ src/strings.js
@@ -0,0 +1,18 @@
+export const TEXT = Object.freeze({
+  REMOVE_FILTER: "Quitar filtro",
+  FILTER_BY_MONTH: "Filtrar por Mes",
+  NO_DATA: "No hay datos para mostrar",
+  IMPORT_DATA: "Importar datos",
+  ORGANIGRAM: "Ver Organigrama",
+  MONTH: "Mes",
+  NAME: "Nombre",
+  ID: "ID",
+  INCOME_DATE: "Fecha de ingreso",
+  GROSS_SALARY: "Sueldo bruto",
+  DIVISION: "División",
+  AREA: "Área",
+  SUBAREA: "Subarea",
+  LEADER_ID: "ID Lider",
+  HIERARCHICAL_LEVEL: "Nivel Jerárquico",
+  SELECT_FILE: "Seleccionar archivo CSV o Excel (.csv, .xlsx, .xls)"
+});
