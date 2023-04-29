15e7a757b98b26da9afa7840463bef44b6d3572a
MauricioMartinez <maoworshipper@gmail.com>
2023-04-29 13:59:52 -0500

    Add filtered data to store

--- src/store/data.js
+++ src/store/data.js
@@ -2,5 +2,17 @@ import { create } from "zustand";
 
 export const useDataStore = create((set) => ({
   data: [],
-  setData: (data) => set({ data }),
+  filteredData: [],
+  setData: (data) => {
+    set({ data });
+    set({ filteredData: data })
+  },
+  setFilteredData: (monthSelected) => {
+    const data = useDataStore.getState().data;
+    const filteredData =
+      monthSelected !== ""
+        ? data.filter((obj) => obj.Mes === monthSelected)
+        : data;
+    set({ filteredData });
+  },
 }));
