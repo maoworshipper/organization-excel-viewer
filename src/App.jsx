3dd86ff068d4a406673d4f0ba84ef651b7eec0a3
MauricioMartinez <maoworshipper@gmail.com>
2023-04-29 14:02:14 -0500

    Modify main App view with new components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import { Input } from "./components/Input/Input";
 import { DataTable } from "./components/DataTable/DataTable";
 import useData from "./hooks/useData";
 import "./App.css";
+import Button from "@mui/material/Button";
+import { SelectComponent as Select } from "./components/Select/Select";
 
 function App() {
   const { importData } = useData();
 
   return (
     <>
+      <Select />
       <Input handleChange={importData} />
       <DataTable />
+      <Button variant="contained">Ver Organigrama</Button>
     </>
   );
 }
