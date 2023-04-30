d5fa7219a903effd31896226b77868d8e1525260
MauricioMartinez <maoworshipper@gmail.com>
2023-04-29 19:09:22 -0500

    Improve text and styles Input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
+import { TEXT } from "../../strings";
 import styles from "./Input.module.scss";
 
 export const Input = ({ handleChange }) => {
   return (
     <div className={styles.container}>
-      <label htmlFor="inputUploadFile">Seleccionar archivo CSV o Excel (.csv, .xlsx, .xls)</label>
+      <label htmlFor="inputUploadFile">{TEXT.ACCEPTED_FORMATS}</label>
       <input
         type="file"
         name="file"
